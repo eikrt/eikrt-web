ceed3f57b528eae6d495ea01f6babb9c0af2f112
Eino Korte <e.i.korte@gmail.com>
2022-05-14 20:13:46 +0300

    added docker stuff

--- frontend/components/layout.js
+++ frontend/components/layout.js
@@ -1,15 +1,14 @@
 import contentStyles from '../styles/content.module.css';
 import styles from './layout.module.css';
-import Link from 'next/link';
 import Head from 'next/head';
 
-export default function Layout({ children }) {
+export default function Layout() {
     return (
 	<div className={styles.container}>
-	    <Head>
-	    <title>Eikrt website </title>
-	    <link rel="icon" href="/favicon.ico"/>
-	    </Head>
+   <Head>
+   <title>Eikrt website </title>
+   <link rel="icon" href="/favicon.ico"/>
+   </Head>
 	<div className={contentStyles.header_box}>
             <div className = {contentStyles.title_box}>
                 <a href="/"> <h1>Eikrt website</h1> </a>
@@ -27,5 +26,5 @@ export default function Layout({ children }) {
                 <a href="/about"> <h1>About</h1> </a>
             </div>
 	</div>
-	    </div>)
+   </div>)
 }
